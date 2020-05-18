b48f82c9c632f1042554491ce8f8ad07ab267285
Metehan <metehaansever@gmail.com>
2020-05-19 02:31:53 +0300

    update

--- singleNumber.py
+++ singleNumber.py
@@ -8,10 +8,8 @@ class Solution(object):
         for i in nums:
             if i in a.keys():
                 a[i]+=1
-                i+=1
             else:
-                a[i]=1
-                i+=1    
+                a[i]=1    
         for key in a:
             if(a[key] == 1):
                 return key
